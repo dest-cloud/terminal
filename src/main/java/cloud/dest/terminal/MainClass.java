00e8aba4bb0b61569a794cfedb26b2f144fca21b
Joël Favre <joel.favre@stemys.io>
2021-03-20 20:31:27 +0100

    The working version

---
+++ src/main/java/cloud/dest/terminal/MainClass.java
@@ -0,0 +1,8 @@
+package cloud.dest.terminal;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        MainApp.main(args);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,10 @@ module terminal {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.kodedu.terminalfx;
+//    requires pty4j;
+//    requires org.apache.commons.lang3;
     requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.databind;
 
     opens cloud.dest.terminal to javafx.fxml;
